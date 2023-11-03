ccac665e973b30605ef514f311c0b86ac4935482
Gonzooo1997 <gonnzalo-gim@hotmail.com>
2023-11-03 17:51:20 +0100

    Add project files.

---
+++ MJU23v_D10_inl_sveng/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MJU23v_D10_inl_sveng
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
